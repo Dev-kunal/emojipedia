dba1d7983005ee47369dbef0c72ee28a8e8ae673
Dev-kunal <68270747+Dev-kunal@users.noreply.github.com>
2021-01-20 21:06:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const emojicollecion = {
+    "😊": "Smiling Emoji",
+    "😂": "Laughining Emoji",
+    "😍": "Loving Emoji",
+    "😎": "Cool Emoji",
+    "😔": "Sad Emoji",
+    "😭": "Crying Emoji",
+    "😮": "Shocked Emoji"
+  };
+
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("Result will Appear Here");
+
+  const emojis = Object.keys(emojicollecion);
+
+  function handleChange(e) {
+    let inputEmoji = e.target.value;
+    setEmoji(inputEmoji);
+
+    if (emojicollecion[inputEmoji]) {
+      setMeaning(emojicollecion[inputEmoji]);
+    } else {
+      setMeaning("Oops..! Emoji Not Found");
+    }
+  }
+
+  function emojiClickHandler(e) {
+    setMeaning(emojicollecion[e]);
+  }
+
+  return (
+    <div className="App">
+      <input
+        value={emoji}
+        onChange={handleChange}
+        placeholder="Insert an Emoji to Know Its Meaning"
+        style={{
+          fontSize: "20px",
+          height: "30px",
+          width: "80vw",
+          textAlign: "center"
+        }}
+      />
+      <h3>{meaning}</h3>
+
+      {emojis.map((e) => (
+        <span
+          key={e}
+          onClick={() => emojiClickHandler(e)}
+          style={{ fontSize: "30px", margin: "5px", cursor: "pointer" }}
+        >
+          {e}
+        </span>
+      ))}
+    </div>
+  );
+}
